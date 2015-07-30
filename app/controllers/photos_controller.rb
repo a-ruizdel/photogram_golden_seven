c0939e679f0e5a74654120ec8a7cd1ca949b1b07
a-ruizdel <a-rdl@wireless-s1-151-5-177.uchicago.edu>
2015-07-29 20:39:47 -0500

    Completes Photogram_golden_seven HWK

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,14 +9,46 @@ class PhotosController < ApplicationController
 
   end
 
+  def create_row
+    # params = {"the_caption"=>"A caption", "the_source"=>"some-url"}
+
+    p = Photo.new
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
 
   def show
-    @index_id = params["id"]
-    @source = Photo.find(params["id"]).source
-    @caption = Photo.find(params["id"]).caption
+    # params = {"id"=>"4"}
 
-    render ("photos/detail.html.erb")
+    @photo = Photo.find(params["id"])
   end
+
+  def destroy
+    @photo = Photo.find(params["id"])
+
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+  @photo = Photo.find(params["id"])
+  render ("photos/edit_form.html.erb")
+  end
+
+
+  def update_row
+    p = Photo.find(params["id"])
+      p.caption = params["the_caption"]
+      p.source = params["the_source"]
+      p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
 
 
 end
