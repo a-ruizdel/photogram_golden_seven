dc11e06ed10bd0008bbaa0dc4adf7e8779476e57
a-ruizdel <a-rdl@wireless-s1-151-5-177.uchicago.edu>
2015-07-29 19:58:32 -0500

    In class work on this project

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+
+  render("photos/new_form.html.erb")
+
+  end
+
+
+  def show
+    @index_id = params["id"]
+    @source = Photo.find(params["id"]).source
+    @caption = Photo.find(params["id"]).caption
+
+    render ("photos/detail.html.erb")
+  end
+
 
 end
